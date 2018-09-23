9c7f483a562a42c3b366bc1dcdd7b852749409f3
is4tomj <tom@is4tomj.com>
2018-09-23 16:38:35 -0700

    added support for profiling

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"flag"
 	"os"
+	"runtime"
+	"runtime/pprof"
+	"log"
 )
-
-
-const nl = byte('\n')
-
-var inputPath = flag.String("input", "" , "input path")
 
 var po = os.Stdout.Write
 var pe = os.Stderr.Write
@@ -20,10 +17,42 @@ func pes(str string) {
 }
 
 func main() {
+
+	// cpu profile stuff
+	cpuprofile := os.Getenv("CPUPROF")
+	if cpuprofile != "" {
+		f, err := os.Create(cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	// content of program
 	switch os.Args[1] {
-  case "gen-creds":
-		genCreds()
+		case "gen-creds":
+			genCreds()
+		case "gen-salts":
+			genSalts()
 	}
 	
 	pes("Done, Byee\n")
+
+	// memory profile stuff
+	memprofile := os.Getenv("MEMPROF")
+	if memprofile != "" {
+		f, err := os.Create(memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		f.Close()
+	}
+
 }
