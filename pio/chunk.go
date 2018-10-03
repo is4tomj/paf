2c021e5531e1247404d9df36daa8d86f0b367c08
is4tomj <tom@is4tomj.com>
2018-10-02 22:38:10 -0700

    lame vscode changes

--- pio/chunk.go
+++ pio/chunk.go
@@ -1,21 +1,20 @@
 package pio
 
 import (
-	"os"
-	"io"
 	"errors"
+	"io"
+	"os"
 )
 
-
-type Chunk struct {	
-	inputPath	string
-	EntryPoint	int64 // in bytes
-	Size		int // in bytes
-	DataSize	int // in bytes
+type Chunk struct {
+	inputPath  string
+	EntryPoint int64 // in bytes
+	Size       int   // in bytes
+	DataSize   int   // in bytes
 }
 
-func (chunk *Chunk)Bytes(buff *[]byte) ([]byte, int, error) {
-	file, err:= os.Open(chunk.inputPath)
+func (chunk *Chunk) Bytes(buff *[]byte) ([]byte, int, error) {
+	file, err := os.Open(chunk.inputPath)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -23,10 +22,10 @@ func (chunk *Chunk)Bytes(buff *[]byte) ([]byte, int, error) {
 
 	// check if byte buffer is already provided
 	if buff == nil || cap(*buff) < chunk.Size {
-		arr := make([]byte, chunk.Size + 4096) // 4096 is a magic number
+		arr := make([]byte, chunk.Size+4096) // 4096 is a magic number
 		buff = &arr
 	}
-	
+
 	// get slice of buffer that is the desired size
 	partial := (*buff)[:chunk.Size]
 
@@ -44,7 +43,6 @@ func (chunk *Chunk)Bytes(buff *[]byte) ([]byte, int, error) {
 	return partial, n, nil
 }
 
-
 // Create Scanner that is '\n' delimited
 func NewLineScanner(b []byte) func() ([]byte, int) {
 	start := -1
@@ -57,9 +55,9 @@ func NewLineScanner(b []byte) func() ([]byte, int) {
 		}
 		for start = end; end < max; end++ {
 			if b[end] == nl { // look for newline char
-				return b[start:end], end-start
+				return b[start:end], end - start
 			}
 		}
-		return b[start:max], max-start
+		return b[start:max], max - start
 	}
 }
